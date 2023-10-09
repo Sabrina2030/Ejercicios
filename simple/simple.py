fc50cd4df84229eeb5057519da81d7cf34f93390
Sabrina <sabrina.rodriguez@yapo.cl>
2023-10-09 16:39:12 -0300

    changes

--- simple/simple.py
+++ simple/simple.py
@@ -1,17 +1,21 @@
 import random
 
+def generate_person():
+    """
+    Generate a random person with 'id' and 'age' attributes.
+    'id' is a unique identifier (integer).
+    'age' is a random integer between 1 and 100.
+    """
+    return {'id': random.randint(1, 1000), 'age': random.randint(1, 100)}
 
 def simple_list():
-    lista = []
-    for i in range(10):
-        diccionario = {
-            'id': i + 1,
-            'age': random.randint(1, 100)
-        }
-        lista.append(diccionario)
-    return lista
+    """
+    Generate a list of 10 random persons using the generate_person function.
+    """
+    return [generate_person() for _ in range(10)]
 
-def sort_list():
-    lista = simple_list()
-    lista.sort(key=lambda x: x['age'])
-    return lista
+def sort_list(persons):
+    """
+    Sort a list of persons by their 'age' attribute in ascending order.
+    """
+    return sorted(persons, key=lambda x: x['age'])
