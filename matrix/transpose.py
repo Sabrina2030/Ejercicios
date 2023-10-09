77caba85d227f4b438c58ec1dfa107ee471885a7
Sabrina <sabrina.rodriguez@yapo.cl>
2023-10-09 17:28:07 -0300

    add transposed exercie

--- matrix/transpose.py
+++ matrix/transpose.py
@@ -1,2 +1,14 @@
 def transpose(matrix):
-    pass
+    if not matrix:
+        return []
+
+    rows = len(matrix)
+    cols = len(matrix[0])
+
+    transposed = [[] for _ in range(cols)]
+
+    for i in range(rows):
+        for j in range(cols):
+            transposed[j].append(matrix[i][j])
+
+    return transposed
